050d3b0eecc2aabda616359d92062421e2ee1cad
FateOrByDesign <s.kulithsamarajeewa@gmail.com>
2026-09-19 07:17:50 +0530

    feat: fetch sheet data and fill missing IATA codes

--- data_manager.py
+++ data_manager.py
@@ -1,3 +1,21 @@
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self, endpoint, token):
+        self.sheety_endpoint = endpoint
+        self.sheety_headers = {"Authorization": token}
+
+    def get_city_from_missing_IATA_records(self, session):
+        """Goes through the google sheet then returns the (row_id, city) of the missing IATA codes"""
+        response = session.get(url=self.sheety_endpoint, headers=self.sheety_headers)
+        data = response.json()["prices"]
+
+        rows_to_be_edited = []
+        for row_index in data:
+            if row_index["iataCode"] == '':
+                rows_to_be_edited.append([row_index["id"], row_index["city"]])
+
+        return rows_to_be_edited
+
+    def set_the_missing_IATA_codes(self, session, row_id, iata_code):
+        response = session.put(url=f"{self.sheety_endpoint}/{row_id}", headers=self.sheety_headers, json={"price": {"iataCode": iata_code}})
+        print(response.text)
--- flight_search.py
+++ flight_search.py
@@ -1,3 +1,15 @@
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
-    pass
+    def __init__(self, api_key, api_endpoint):
+        self.serpApi_key = api_key
+        self.serpApi_endpoint = api_endpoint
+
+    def get_iata_codes(self,session, city):
+        query_params = {
+            "engine": "google_flights_autocomplete",
+            "api_key": self.serpApi_key,
+            "q": city
+        }
+
+        response = session.get(url=self.serpApi_endpoint, params=query_params)
+        return response.json()
